b5f105cc07a9df6873cb3459623ad571cca1d054
yigitmn <109553125+yigitmn@users.noreply.github.com>
2022-07-27 10:53:14 +0300

    Create ödev.ensaryigit.py

---
+++ ödev.ensaryigit.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 21 18:23:38 2022
+
+@author: ensar
+"""
+
+#soru-1
+print("Merhaba Dünya")
+
+##############################################################
+#soru2
+ad = input("lütfen isiminizi giriniz:")
+print("Merhaba {}".format(ad ))
+##############################################################
+#soru-3
+a = int(input("a sayısını giriniz:"))
+b = int(input("b sayısını giriniz:"))
+print("a+b =",a+b)
+##############################################################
+#soru-4
+x = int(input("x sayısını giriniz:"))
+y = int(input("y sayısını giriniz:"))
+print("x ve y nin ortalaması:",(x+y)/2)
+##############################################################
