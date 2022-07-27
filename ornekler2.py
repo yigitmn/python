e9686d1c07e4cdf3fad5cecb278cac3c33ac670c
yigitmn <109553125+yigitmn@users.noreply.github.com>
2022-07-27 13:03:42 +0300

    Create ornekler2.py

---
+++ ornekler2.py
@@ -0,0 +1,85 @@
+"""
+ad = input("adınızı giriniz :")
+soyad = input("soyadınzı giriniz :")
+yas = input("yaşınızı giriniz :")
+dogumyeri = input("doğum yerinizi  giriniz :")
+kardesayisi = input("kardeş sayınızı giriniz :")
+anaadı = input("anne adınızı giriniz :")
+babaadi = input("baba adınızı giriniz giriniz :")
+ders = input("en sevdiğiniz dersi giriniz :")
+oyun = input("en sevdiğiniz oyunu giriniz :")
+yemek = input("en sevdiğiniz yemeği giriniz :")
+
+
+print("adı:",ad)
+print("soyadı:",soyad)
+print("yaşınızı:",yas)
+print("doğum yeri:",dogumyeri)
+print("anne adı:",anaadı)
+print("baba adı:",babaadi)
+print("kardeş sayısı:",kardesayisi)
+print("adı:",ders)
+print("adı:",oyun)
+print("adı:",yemek)
+"""
+###############################################################################
+"""
+ad = input("kullanici adınızı giriniz:")
+parola = input("parolanızı giriniz:")
+print(ad == "fatih" and parola == "29mayıs1453")
+fetih = 2022 - 1453
+print("İstanbul fethedileli geçen yıl:",fetih)
+"""
+###############################################################################
+"""
+puan = int(input("sınav notunuzu giriniz :"))
+   
+if ( puan >= 85):
+    print("pekiyi")
+elif(puan >= 70 and puan<= 84):
+    print("iyi")
+elif(puan >= 60 and puan<= 69):
+    print("orta")
+elif(puan<=59 and puan>=50):
+    print("geçer")
+elif(puan<=49 and puan>=25):
+    print("çalışmalısın")
+else:
+    print("sanayiye git")
+"""    
+###############################################################################
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
