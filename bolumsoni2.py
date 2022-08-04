a3e6227e1bbfcbd8d4ac58d166366bd14e642fac
yigitmn <109553125+yigitmn@users.noreply.github.com>
2022-08-04 11:14:02 +0300

    Create bolumsoni2.py

---
+++ bolumsoni2.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  4 10:33:13 2022
+
+@author: ensar
+"""
+
+#soru-1
+"""
+harfler = ["a","e","i","i","o","ö","u","ü"]
+harfler.count("i")
+harfler.count("p")
+"""
+
+#soru-2
+"""
+liste1,liste2,liste3,liste4 = ["a",1,"b",2]
+print(liste1)
+print(liste2)
+print(liste3)
+print(liste4)
+"""
+
+#soru-3
+"""
+liste = ["a"]
+listee = ["b"]
+listeee = liste +listee
+print(listeee)
+"""
+
+#soru-4
+"""
+liste = [34,1,566,334,23,2,3,19]
+liste.sort()
+print(liste)
+liste.reverse()
+print(liste)
+"""
+
+#soru-5
+"""
+Tebrikler,Türkiye,Merhaba
+"""
+
+#soru-6
+"""
+listes = [1,2,3,4,5,6,7]
+print(listes[1:3])
+print(listes[0:3])
+print(listes[3:])
+print(listes[:7])
+"""
+
+#soru-7
+"""
+isimler = ["ali","veli","ayşe"]
+soyisimler = ["Türk","İzci","Erel"]
+ad_soyad1 = isimler[0]+ " " +soyisimler[0]
+ad_soyad2 = isimler[1]+ " " +soyisimler[1]
+ad_soyad3 = isimler[2]+ " " +soyisimler[2]
+print(ad_soyad1)
+print(ad_soyad2)
+print(ad_soyad3)
+"""
+
+#soru-8
+"""
+liste = ["bir","iki","dört"]
+print(liste)
+liste.insert(2, "üç")
+liste.insert(4, "beş")
+print(liste)
+"""
+
+
+#soru-9
+"""
+liste = ["birinci veri","ikinci veri","üçüncü veri","dördüncü veri","beşinci veri"]
+len(liste)
+print(liste[0])
+print(liste[4])
+"""
